2fb2730579d83de36afe124e27cbb090481c1f59
tomfennelly <tomfennelly@58448c06-5d2c-0410-b55c-e165fbcd3e60>
2005-12-19 18:01:01 +0000

    updated

--- cdres-packages/smooks-report-0.1/bin/report.cmd
+++ cdres-packages/smooks-report-0.1/bin/report.cmd
@@ -2,7 +2,10 @@
 
 set JAVA_HOME=\j2sdk1.4.2_03
 
- -e [encoding] -r -d [output-folder] -b [base-URL] -s [start-page]
+set ENCODING=ISO-8859-1
+set RECURSIVE=-r
+set OUTFOLDER=\zap
+set BASEURL=http://localhost:8080/smooksreport
 
 @rem Check for JAVA_HOME.
 if not exist "%JAVA_HOME%" (@echo. && @echo. && @echo Sorry, JAVA_HOME environment variable must be set in "%~dp0env.cmd"!! && goto end)
@@ -11,9 +14,9 @@ set SMOOKS_HOME=%~dp0..
 rem set LIB=%SMOOKS_HOME%/lib
 set LIB=H:\projects\codehaus\smooks\build
 
-set CLASSPATH=%LIB%/milyn-smooks-0.5.jar;%LIB%/xercesImpl.jar;%SMOOKS_HOME%/lib/batik-css.jar;%LIB%/spring-1.1.2-core.jar;%LIB%/commons-logging1.0.4.jar
+set CLASSPATH=%LIB%/milyn-smooks-0.5.jar;%LIB%/xercesImpl.jar;%LIB%/spring-1.1.2-core.jar;%LIB%/commons-logging1.0.4.jar;%LIB%/bsh-core-2.0b4.jar
 if exist "%SMOOKS_HOME%/classes" (set CLASSPATH=%SMOOKS_HOME%/classes;%CLASSPATH%)
 
-%JAVA_HOME%\bin\java -DSMOOKS_HOME=%SMOOKS_HOME% org.milyn.report.SmooksReportGenerator -r %1 %2 %3 %4 %5 %6 %7 %8 %9
+%JAVA_HOME%\bin\java -DSMOOKS_HOME=%SMOOKS_HOME% org.milyn.report.SmooksReportGenerator -e %ENCODING% %RECURSIVE% -d %OUTFOLDER% -b %BASEURL% %1 %2 %3 %4 %5 %6 %7 %8 %9
 
 :end
