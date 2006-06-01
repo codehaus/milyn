c8b0c2d533a6dab9b95029074663a3df05159dd9
tomfennelly <tomfennelly@58448c06-5d2c-0410-b55c-e165fbcd3e60>
2006-06-01 10:44:26 +0000

    added templating cartridge

---
+++ trunk/smooks-cartridges/templating/src/main/java/org/milyn/templating/TemplatingUtils.java
@@ -0,0 +1,59 @@
+/*
+	Milyn - Copyright (C) 2006
+
+	This library is free software; you can redistribute it and/or
+	modify it under the terms of the GNU Lesser General Public
+	License (version 2.1) as published by the Free Software 
+	Foundation.
+
+	This library is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+    
+	See the GNU Lesser General Public License for more details:    
+	http://www.gnu.org/licenses/lgpl.txt
+*/
+
+package org.milyn.templating;
+
+import java.io.InputStream;
+
+import org.milyn.SmooksStandalone;
+import org.milyn.cdr.SmooksConfigurationException;
+import org.milyn.container.ContainerContext;
+
+/**
+ * Utility class for the templating Cartridge.
+ * @author tfennelly
+ */
+public class TemplatingUtils {
+    
+    private static final String TEMPLATING_CDU_CREATORS_CDRL = "templating-cdu-creators-0.1.cdrl";
+
+    /**
+     * Register the Templating {@link org.milyn.delivery.ContentDeliveryUnitCreator}
+     * implementations defined by this Cartrdge with the suppied Smooks
+     * {@link ContainerContext} store.
+     * <p/>
+     * This method can be used as a convienient way of registering these resources
+     * when using e.g. {@link org.milyn.SmooksStandalone}. Example:
+     * <pre>
+     * SmooksStandalone smooks = new SmooksStandalone("UTF-8");
+     * 
+     * TemplatingUtils.registerCDUCreators(smooks.{@link org.milyn.SmooksStandalone#getContext() getContext()}); 
+     * </pre>
+     * <p/>
+     * See {@link org.milyn.templating.stringtemplate.StringTemplateContentDeliveryUnitCreator}
+     * and {@link org.milyn.templating.xslt.XslContentDeliveryUnitCreator} as examples. 
+     * @param context The context on which the {@link org.milyn.delivery.ContentDeliveryUnitCreator}
+     * are to be registered.
+     */
+    public static void registerCDUCreators(ContainerContext context) {
+        InputStream stream = TemplatingUtils.class.getResourceAsStream(TEMPLATING_CDU_CREATORS_CDRL);
+        try {
+            context.getStore().registerResources(TEMPLATING_CDU_CREATORS_CDRL, stream);
+        } catch (Exception e) {
+            throw new SmooksConfigurationException("Failed to load classpath resource file: " + TEMPLATING_CDU_CREATORS_CDRL + ".  Should be in package: " + TemplatingUtils.class.getPackage().getName(), e);
+        }
+    }
+}
---
+++ trunk/smooks-cartridges/templating/src/main/java/org/milyn/templating/stringtemplate/StringTemplateContentDeliveryUnitCreator.java
@@ -0,0 +1,133 @@
+/*
+	Milyn - Copyright (C) 2006
+
+	This library is free software; you can redistribute it and/or
+	modify it under the terms of the GNU Lesser General Public
+	License (version 2.1) as published by the Free Software 
+	Foundation.
+
+	This library is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+    
+	See the GNU Lesser General Public License for more details:    
+	http://www.gnu.org/licenses/lgpl.txt
+*/
+
+package org.milyn.templating.stringtemplate;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+
+import org.antlr.stringtemplate.StringTemplate;
+import org.antlr.stringtemplate.StringTemplateGroup;
+import org.milyn.cdr.SmooksConfigurationException;
+import org.milyn.cdr.SmooksResourceConfiguration;
+import org.milyn.container.ContainerRequest;
+import org.milyn.delivery.ContentDeliveryUnit;
+import org.milyn.delivery.ContentDeliveryUnitCreator;
+import org.milyn.delivery.process.AbstractProcessingUnit;
+import org.milyn.dom.DomUtils;
+import org.milyn.javabean.BeanAccessor;
+import org.w3c.dom.Element;
+import org.w3c.dom.Text;
+
+
+/**
+ * StringTemplate {@link org.milyn.delivery.process.ProcessingUnit} ConcreteCreator class (GoF - Factory Method).
+ * <p/>
+ * Creates {@link org.milyn.delivery.ContentDeliveryUnit} instances for applying
+ * <a href="http://www.stringtemplate.org/">StringTemplate</a> transformations.
+ * <p/>
+ * This templating solution relies on the <a href="http://milyn.codehaus.org/downloads">Smooks JavaBean Cartridge</a>
+ * to perform the JavaBean population that's required by <a href="http://www.stringtemplate.org/">StringTemplate</a>.
+ * @author tfennelly
+ */
+public class StringTemplateContentDeliveryUnitCreator implements ContentDeliveryUnitCreator {
+
+    /**
+     * Public constructor.
+     * @param config Configuration details for this ContentDeliveryUnitCreator.
+     */
+    public StringTemplateContentDeliveryUnitCreator(SmooksResourceConfiguration config) {        
+    }
+    
+	/**
+	 * Create a StringTemplate based ContentDeliveryUnit.
+     * @param resourceConfig The SmooksResourceConfiguration for the StringTemplate.
+     * @return The StringTemplate {@link ContentDeliveryUnit} instance.
+	 */
+	public synchronized ContentDeliveryUnit create(SmooksResourceConfiguration resourceConfig) throws InstantiationException {
+        return new StringTemplateProcessingUnit(resourceConfig);
+	}
+
+	/**
+	 * StringTemplate template application ProcessingUnit.
+	 * @author tfennelly
+	 */
+	private static class StringTemplateProcessingUnit extends AbstractProcessingUnit {
+
+        private boolean visitBefore = false;
+        private StringTemplate template;
+        
+        public StringTemplateProcessingUnit(SmooksResourceConfiguration config) {
+            super(config);
+            visitBefore = config.getBoolParameter("visitBefore", false);
+            
+            String path = config.getPath();
+            String encoding = config.getStringParameter("encoding", "UTF-8");
+            
+            if(path.charAt(0) != '/') {
+                path = "/" + path;
+            }
+            
+            InputStream templateResStream = getClass().getResourceAsStream(path);
+            if(templateResStream == null) {
+                throw new SmooksConfigurationException("Unable to load StringTemplate resource stream: " + path);
+            }
+            
+            SmooksStringTemplateGroup templateGroup = new SmooksStringTemplateGroup();
+            template = templateGroup.loadTemplate(path, templateResStream, encoding);        
+        }
+
+        public void visit(Element element, ContainerRequest request) {
+            // First thing we do is clone the template for this transformation...
+            StringTemplate thisTransTemplate = template.getInstanceOf();
+            HashMap beans = BeanAccessor.getBeans(request);
+            String transformedElementResult;
+
+            // Set the document data beans on the template and apply it...
+            thisTransTemplate.setAttributes(beans);
+            transformedElementResult = thisTransTemplate.toString();
+            
+            // Create the replacement DOM text node containing the applied template...            
+            Text transformedElement = element.getOwnerDocument().createTextNode(transformedElementResult);
+            DomUtils.replaceNode(transformedElement, element);
+        }
+
+        public boolean visitBefore() {
+            return visitBefore;
+        }
+	}
+    
+    private static class SmooksStringTemplateGroup extends StringTemplateGroup {
+        private SmooksStringTemplateGroup() {
+            super("smooks-st-group");
+        }
+        
+        private StringTemplate loadTemplate(String name, InputStream stream, String encoding) {
+            try {
+                InputStreamReader reader = new InputStreamReader(stream, encoding);
+                return loadTemplate(name, new BufferedReader(reader));
+            } catch (UnsupportedEncodingException e) {
+                throw new SmooksConfigurationException("Invalid StringTemplate template encoding configuration.", e);
+            } catch (IOException e) {
+                throw new SmooksConfigurationException("Unable to read StringTemplate template: " + name, e);
+            }
+        }
+    }
+}
