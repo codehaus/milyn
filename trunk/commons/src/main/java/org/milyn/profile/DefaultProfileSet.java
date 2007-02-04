9e823381d7fc6dc9319f3108c819066a31762e62
tomfennelly <tomfennelly@58448c06-5d2c-0410-b55c-e165fbcd3e60>
2007-02-04 11:48:31 +0000

    Moved profiling and useragent code from tinak to commons

---
+++ trunk/commons/src/main/java/org/milyn/profile/DefaultProfileSet.java
@@ -0,0 +1,113 @@
+/*
+	Milyn - Copyright (C) 2006
+
+	This library is free software; you can redistribute it and/or
+	modify it under the terms of the GNU Lesser General Public
+	License (version 2.1) as published by the Free Software 
+	Foundation.
+
+	This library is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+    
+	See the GNU Lesser General Public License for more details:    
+	http://www.gnu.org/licenses/lgpl.txt
+*/
+
+package org.milyn.profile;
+
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+
+/**
+ * Default ProfileSet implementation.
+ * @author tfennelly
+ */
+public class DefaultProfileSet extends LinkedHashMap implements ProfileSet {
+
+    private static final long serialVersionUID = 1L;
+
+    /* (non-Javadoc)
+	 * @see org.milyn.useragent.profile.ProfileSet#isMember(java.lang.String)
+	 */
+	public boolean isMember(String profile) {
+		if(profile == null) {
+			throw new IllegalArgumentException("null 'profile' arg in method call.");
+		}
+
+		return containsKey(profile.trim().toLowerCase());
+	}
+
+	/**
+	 * Add profile to the ProfileSet.
+	 * @param profile The profile to add.
+	 */
+	public void addProfile(String profile) {
+		addProfile(new BasicProfile(profile.toLowerCase()));
+	}
+
+	/**
+	 * Add profile to the ProfileSet.
+	 * @param profile The profile to add.
+	 */
+	@SuppressWarnings("unchecked")
+	public void addProfile(Profile profile) {
+		if(profile == null) {
+			throw new IllegalArgumentException("null 'profile' arg in method call.");
+		}
+
+		put(profile.getName().toLowerCase(), profile);
+	}	
+
+	/**
+	 * Get a profile from the {@link ProfileSet}.
+	 * @param profile The name of the profile.
+	 * @return The requested Profile, or null if the profile is not a
+	 * member of the {@link ProfileSet}.
+	 */
+	public Profile getProfile(String profile) {
+		return (Profile)get(profile);
+	}
+
+	/**
+	 * Get an {@link Iterator} to allow iteration over the 
+	 * {@link Profile Profiles}in this {@link ProfileSet}.
+	 * @return An {@link Iterator} that allows iteration over the 
+	 * {@link Profile Profiles}in this {@link ProfileSet}.
+	 */
+	public Iterator iterator() {
+		return values().iterator();
+	}
+	
+	/**
+	 * Add the profiles of the supplied DefaultProfileSet to this ProfileSet.
+	 * @param profileSet The DefaultProfileSet whose profiles are to be added.
+	 */
+	@SuppressWarnings("unchecked")
+	protected void addProfileSet(DefaultProfileSet profileSet) {
+		if(profileSet == null) {
+			throw new IllegalArgumentException("null 'profileSet' arg in method call.");
+		}
+		
+		putAll(profileSet);
+	}
+	
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	public String toString() {
+		StringBuffer setDescription = new StringBuffer();
+		Iterator iterator = keySet().iterator();
+		
+		while(iterator.hasNext()) {
+			String profile = (String)iterator.next();
+			
+			setDescription.append(profile);
+			if(iterator.hasNext()) {
+				setDescription.append(",");
+			}
+		}
+		
+		return setDescription.toString();
+	}
+}
---
+++ trunk/commons/src/main/java/org/milyn/profile/ProfileStore.java
@@ -0,0 +1,53 @@
+/*
+	Milyn - Copyright (C) 2006
+
+	This library is free software; you can redistribute it and/or
+	modify it under the terms of the GNU Lesser General Public
+	License (version 2.1) as published by the Free Software 
+	Foundation.
+
+	This library is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+    
+	See the GNU Lesser General Public License for more details:    
+	http://www.gnu.org/licenses/lgpl.txt
+*/
+
+package org.milyn.profile;
+
+
+/**
+ * ProfileStore interface.
+ * <p/>
+ * The ProfileStore stores and provides access to all the ProfileSet instances.
+ * A ProfileSet is the set of profiles associated with a given "profile member".  
+ * A "profile member" can be a member of multiple profiles.  This is what "profiling" is
+ * all about - dicing and slicing entity sets into different groups by assigning
+ * them to profiles.
+ * 
+ * @author tfennelly
+ */
+public interface ProfileStore {
+
+	/**
+	 * Add a ProfileSet for the named profile member.
+	 * @param deviceName The device name.
+	 * @param profileSet The devices ProfileSet.
+	 */
+	public void addProfileSet(String deviceName, ProfileSet profileSet);
+	
+	/**
+	 * Get the ProfileSet associated with the specified profile member.
+	 * <p/>
+	 * A profile "member" can be a member of multiple profiles.  This method 
+	 * should return that list of profiles.
+	 * <p/>
+	 * Implementations must be case insensitive.
+	 * @param profileMember The device name.
+	 * @return The ProfileSet for the specified device.
+	 * @throws UnknownProfileMemberException There's no {@link ProfileSet} for
+	 * the specified profile memeber.
+	 */
+	public ProfileSet getProfileSet(String profileMember) throws UnknownProfileMemberException;
+}
