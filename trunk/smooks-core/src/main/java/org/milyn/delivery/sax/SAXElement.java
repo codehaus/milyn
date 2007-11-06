dcc6801989a7a2be6620125838e7dcd732075c91
tomfennelly <tomfennelly@58448c06-5d2c-0410-b55c-e165fbcd3e60>
2007-11-06 19:03:44 +0000

    Updated maven pom versions

--- trunk/smooks-core/src/main/java/org/milyn/delivery/sax/SAXElement.java
+++ trunk/smooks-core/src/main/java/org/milyn/delivery/sax/SAXElement.java
@@ -15,24 +15,49 @@
 */
 package org.milyn.delivery.sax;
 
+import org.milyn.assertion.AssertArgument;
 import org.xml.sax.Attributes;
 
 import javax.xml.namespace.QName;
-import java.io.Serializable;
+import java.io.Writer;
 
 /**
  * Element details as described by the SAX even model API.
  * <p/>
  * {@link org.milyn.delivery.sax.SAXElementVisitor} implementations will be passed
- * an instance of this class for each of the 
+ * an instance of this class for each of the event methods of
+ * {@link org.milyn.delivery.sax.SAXElementVisitor}.
+ *
+ * <h3 id="element_cache_object">Element Cache Object</h3>
+ * This class supports the concept of a "cache" object which can be get and set through
+ * the {@link #getCache()} and {@link #setCache(Object)}.  The cache object can be used by
+ * {@link org.milyn.delivery.sax.SAXElementVisitor} implementations to store information
+ * between calls to the {@link org.milyn.delivery.sax.SAXElementVisitor} event methods.
+ * <p/>
+ * Obviously we could have implemented this cache as a straightforward {@link java.util.Map},
+ * but that forces you to do silly things in situations where you wish to (for example)
+ * store a {@link java.util.List}, or a single Object reference of some other type.
+ *
+ * <h3 id="element-writing">Element Writing/Serialization</h3>
+ * Each SAXElement instance has a {@link Writer writer} set on it.
+ * {@link org.milyn.delivery.sax.SAXElementVisitor} implementations can take care of
+ * serializing the elements at which they are targeted themselves.  Alternatively, they
+ * can use the {@link org.milyn.delivery.sax.WriterUtil} class.
+ * <p/>
+ * {@link org.milyn.delivery.sax.SAXElementVisitor} can also control the serialization
+ * of their "child elements" by {@link #setWriter(java.io.Writer) setting the writter}
+ * on the SAXElement instance they receive.  This works because Smooks passes the
+ * writer instance that's set on a SAXElement instance to all of the SAXElement
+ * instances created for child elements. 
  *
  * @author <a href="mailto:tom.fennelly@gmail.com">tom.fennelly@gmail.com</a>
  */
-public class SAXElement implements Serializable {
+public class SAXElement {
 
     private QName name;
     private Attributes attributes;
     private Object cache;
+    private Writer writer;
 
     /**
      * Public constructor.
@@ -67,6 +92,28 @@ public class SAXElement implements Serializable {
     }
 
     /**
+     * Get the writer to which this element should be writen to.
+     * <p/>
+     * See <a href="#element-writing">element writing</a>.
+     *
+     * @return The element writer.
+     */
+    public Writer getWriter() {
+        return writer;
+    }
+
+    /**
+     * Set the writer to which this element should be writen to.
+     * <p/>
+     * See <a href="#element-writing">element writing</a>.
+     * 
+     * @param writer The element writer.
+     */
+    public void setWriter(Writer writer) {
+        this.writer = writer;
+    }
+
+    /**
      * Get the element naming details.
      * @return Element naming details.
      */
@@ -74,7 +121,12 @@ public class SAXElement implements Serializable {
         return name;
     }
 
+    /**
+     * Set the naming details for the Element.
+     * @param name The element naming details.
+     */
     public void setName(QName name) {
+        AssertArgument.isNotNull(name, "name");
         this.name = name;
     }
 
@@ -86,14 +138,29 @@ public class SAXElement implements Serializable {
         return attributes;
     }
 
+    /**
+     * Set the element attributes.
+     * @param attributes The element attributes.
+     */
     public void setAttributes(Attributes attributes) {
+        AssertArgument.isNotNull(attributes, "attributes");
         this.attributes = attributes;
     }
 
+    /**
+     * Get the <a href="#element_cache_object">element cache object</a>.
+     *
+     * @return The element cache Object.
+     */
     public Object getCache() {
         return cache;
     }
 
+    /**
+     * Set the <a href="#element_cache_object">element cache object</a>.
+     *
+     * @param cache The element cache Object.
+     */
     public void setCache(Object cache) {
         this.cache = cache;
     }
---
+++ trunk/smooks-core/src/main/java/org/milyn/delivery/sax/WriterUtil.java
@@ -0,0 +1,48 @@
+/*
+	Milyn - Copyright (C) 2006
+
+	This library is free software; you can redistribute it and/or
+	modify it under the terms of the GNU Lesser General Public
+	License (version 2.1) as published by the Free Software
+	Foundation.
+
+	This library is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+	See the GNU Lesser General Public License for more details:
+	http://www.gnu.org/licenses/lgpl.txt
+*/
+package org.milyn.delivery.sax;
+
+/**
+ * {@link SAXElement} writing/serialization utility class.
+ * 
+ * @author <a href="mailto:tom.fennelly@gmail.com">tom.fennelly@gmail.com</a>
+ */
+public class WriterUtil {
+
+    public static void writeEmptyElement(SAXElement element) {
+
+    }
+
+    public static void writeStartElement(SAXElement element) {
+
+    }
+
+    public static void writeText(SAXElement element, String text) {
+
+    }
+
+    public static void writeComment(SAXElement element, String comment) {
+
+    }
+
+    public static void writeCDATA(SAXElement element, String cdata) {
+
+    }
+
+    public static void writeEndElement(SAXElement element) {
+
+    }
+}
