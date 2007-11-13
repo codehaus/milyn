759074f5e94da4b1f692535949a59ede9904f531
danielbevenius <danielbevenius@58448c06-5d2c-0410-b55c-e165fbcd3e60>
2007-11-13 14:38:10 +0000

    Work for Jira : MILYN-61 "IntegerDecoder should be able to handle en empty data string"

---
+++ trunk/smooks-core/src/test/java/org/milyn/javabean/decoders/IntegerDecodingTest.java
@@ -0,0 +1,34 @@
+/*
+	Milyn - Copyright (C) 2006
+
+	This library is free software; you can redistribute it and/or
+	modify it under the terms of the GNU Lesser General Public
+	License (version 2.1) as published by the Free Software
+	Foundation.
+
+	This library is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+	See the GNU Lesser General Public License for more details:
+	http://www.gnu.org/licenses/lgpl.txt
+*/
+package org.milyn.javabean.decoders;
+
+import junit.framework.TestCase;
+
+/**
+ * Tests for the Calendar and Date decoders.
+ * 
+ * @author <a href="mailto:daniel.bevenius@gmail.com">daniel.bevenius@gmail.com</a>
+ */
+public class IntegerDecodingTest extends TestCase {
+	private IntegerDecoder decoder = new IntegerDecoder();
+
+    public void test_CalendarDecoder() {
+        Object decode = decoder.decode( "" );
+        assertTrue( decode instanceof Integer);
+        assertEquals( new Integer(0), (Integer)decode);
+    }
+
+}
