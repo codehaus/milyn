d4f9ad0753cb7730e1b89f3e2bbb0f188f76d443
danielbevenius <danielbevenius@58448c06-5d2c-0410-b55c-e165fbcd3e60>
2007-11-27 07:28:20 +0000

    Clean up.

--- trunk/smooks-examples/sj-testimonial/src/main/java/se/sj/ipl/rollingstock/domain/Comment.java
+++ trunk/smooks-examples/sj-testimonial/src/main/java/se/sj/ipl/rollingstock/domain/Comment.java
@@ -15,14 +15,18 @@ public class Comment implements Serializable
 	{
 		if ( comment == null)
 			throw new IllegalArgumentException("comment must not be null");		
-		this.comment = comment;
+		this.comment = comment.trim();
 	}
 	
 	public int getId() { return id; }
 	public void setId(int id) { this.id = id; }
 	
 	public String getComment() { return comment; }
-	public void setComment(String comment) { this.comment = comment; }
+	public void setComment(String comment) 
+	{ 
+		if ( comment != null)
+			this.comment = comment.trim(); 
+	}
 	
 	@Override
 	public boolean equals(Object obj)
