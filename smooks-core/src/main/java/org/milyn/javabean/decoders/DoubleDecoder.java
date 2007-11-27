e230ceb66c0ee773b9f061c107dfaca91881a65e
danielbevenius <danielbevenius@58448c06-5d2c-0410-b55c-e165fbcd3e60>
2007-11-27 08:30:59 +0000

    Added a test for null and empty strings for the data argument.

--- smooks-core/src/main/java/org/milyn/javabean/decoders/DoubleDecoder.java
+++ smooks-core/src/main/java/org/milyn/javabean/decoders/DoubleDecoder.java
@@ -3,8 +3,6 @@ package org.milyn.javabean.decoders;
 import org.milyn.javabean.DataDecoder;
 import org.milyn.javabean.DataDecodeException;
 import org.milyn.javabean.DecodeType;
-import org.milyn.cdr.SmooksResourceConfiguration;
-import org.milyn.cdr.SmooksConfigurationException;
 
 /**
  * Double decoder.
@@ -15,6 +13,8 @@ import org.milyn.cdr.SmooksConfigurationException;
 public class DoubleDecoder implements DataDecoder {
 
     public Object decode(String data) throws DataDecodeException {
+    	if ( data == null || data.equals(""))
+    		return new Double(0.0);
         try {
             return Double.parseDouble(data.trim());
         } catch(NumberFormatException e) {
