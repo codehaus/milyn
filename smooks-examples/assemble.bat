d5ca4ee6630aaac1d948d009bc43ea8d0960dce0
tomfennelly <tomfennelly@58448c06-5d2c-0410-b55c-e165fbcd3e60>
2008-01-21 13:08:24 +0000

    updates

--- smooks-examples/assemble.bat
+++ smooks-examples/assemble.bat
@@ -1,24 +1,28 @@
 pushd css
-call call mvn assembly:assembly
-popd
-
-pushd csv-to-xml
-call call mvn assembly:assembly
-popd
-
-pushd edi-to-java
-call call mvn assembly:assembly
-popd
-
-pushd edi-to-xml
-call call mvn assembly:assembly
-popd
-
-pushd xml-to-java-basic
 call mvn assembly:assembly
 popd
 
-pushd javabean-populator
+pushd csv-to-xml
+call mvn assembly:assembly
+popd
+
+pushd edi-to-java
+call mvn assembly:assembly
+popd
+
+pushd edi-to-xml
+call mvn assembly:assembly
+popd
+
+pushd java-basic
+call mvn assembly:assembly
+popd
+
+pushd java-to-java
+call mvn assembly:assembly
+popd
+
+pushd java-to-xml
 call mvn assembly:assembly
 popd
 
@@ -26,7 +30,31 @@ pushd model-driven-basic
 call mvn assembly:assembly
 popd
 
+pushd model-driven-basic-virtual
+call mvn assembly:assembly
+popd
+
+pushd mule-esb-integration
+call mvn assembly:assembly
+popd
+
 pushd profiling
+call mvn assembly:assembly
+popd
+
+pushd servlet\war
+call mvn assembly:assembly
+popd
+
+pushd sj-testimonial
+call mvn assembly:assembly
+popd
+
+pushd xml-to-java
+call mvn assembly:assembly
+popd
+
+pushd xml-to-java-virtual
 call mvn assembly:assembly
 popd
 
@@ -34,18 +62,6 @@ pushd xslt-basic
 call mvn assembly:assembly
 popd
 
-pushd xslt-complex
-call mvn assembly:assembly
-popd
-
 pushd xslt-groovy
-call mvn assembly:assembly
-popd
-
-pushd xslt-namespaces
-call mvn assembly:assembly
-popd
-
-pushd servlet\war
 call mvn assembly:assembly
 popd
