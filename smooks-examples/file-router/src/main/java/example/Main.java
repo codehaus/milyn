d90e0a4b84731523dda9be74009a1a6fbcdc2123
danielbevenius <danielbevenius@58448c06-5d2c-0410-b55c-e165fbcd3e60>
2008-03-20 08:55:00 +0000

    Updated exemple...still needs some work but I'd like to see if Tom can run this as there were issues with building.

--- smooks-examples/file-router/src/main/java/example/Main.java
+++ smooks-examples/file-router/src/main/java/example/Main.java
@@ -17,10 +17,12 @@ package example;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
 import java.io.StringWriter;
+import java.io.Writer;
 import java.util.List;
 
 import javax.xml.transform.stream.StreamResult;
@@ -29,39 +31,52 @@ import org.milyn.Smooks;
 import org.milyn.SmooksException;
 import org.milyn.container.ExecutionContext;
 import org.milyn.delivery.java.JavaSource;
-import org.milyn.io.StreamUtils;
+import org.milyn.event.report.HtmlReportGenerator;
 import org.milyn.javabean.BeanAccessor;
 import org.milyn.routing.file.FileListAccessor;
-import org.milyn.routing.file.FileRouter;
 import org.xml.sax.SAXException;
 
 import example.model.Order;
 
 /**
- *
+ * This is a simple example that demonstrates how Smooks can be 
+ * configured to "route" the output of a transform to file(s).
+ * 
  * @author <a href="mailto:daniel.bevenius@gmail.com">Daniel Bevenius</a>
  *
  */
 public class Main
 {
-    protected String runSmooksTransform(final Object order) throws IOException, SAXException
+	private static final String LINE_SEP = System.getProperty( "line.separator" );
+	
+    protected void runSmooksTransform( final Object order) throws IOException, SAXException, ClassNotFoundException
     {
         final Smooks smooks = new Smooks("smooks-config.xml");
         final ExecutionContext executionContext = smooks.createExecutionContext();
+        
+        // 	manually inserting the Order bean into the execution context
         BeanAccessor.addBean( executionContext, "order", order );
 
-        try
-        {
-            final StringWriter writer = new StringWriter();
-            smooks.filter(new JavaSource(order), new StreamResult(writer), executionContext);
-        }
-        finally
-        {
-        }
-        System.out.println("\n\n");
-        System.out.println( "Transformed and appended file name to file : " + FileListAccessor.getFileName( executionContext ) );
+        //	generate a smooks-report.html
+    	Writer reportWriter = new FileWriter("smooks-report.html");
+    	executionContext.setEventListener(new HtmlReportGenerator(reportWriter));
+    	
+    	//	perform the transform
+        final StringWriter writer = new StringWriter();
+        smooks.filter(new JavaSource(order), new StreamResult(writer), executionContext);
+        
+        //	display the output from the transform
+        System.out.println( LINE_SEP );
+        System.out.println( "List file : [" + FileListAccessor.getFileName( executionContext ) + "]" );
         List<String> fileNames = (List<String>) FileListAccessor.getFileList( executionContext );
-        return fileNames.get( 0 );
+        for (String fileName : fileNames)
+		{
+            System.out.println( LINE_SEP );
+            System.out.println( "fileName : [" + fileName + "] content:" );
+            System.out.println( new ObjectInputStream( new FileInputStream( fileName ) ).readObject() );
+            System.out.println();
+			
+		}
     }
 
     public static void main(String[] args) throws IOException, SAXException, SmooksException, InterruptedException, ClassNotFoundException
@@ -70,17 +85,13 @@ public class Main
         final Order order = new Order();
 
         pause("Press 'enter' to display the input Order Object...");
-        System.out.println("\n");
+        System.out.println( LINE_SEP );
         System.out.println( order );
-        System.out.println("\n\n");
-
+        System.out.println( LINE_SEP );
         System.out.println("This needs to be transformed and appended to a file");
         pause("Press 'enter' to display the transformed message...");
-        String fileName = smooksMain.runSmooksTransform(order);
-        System.out.println("\n\n");
-        System.out.println( "File contents :");
-        System.out.println( new ObjectInputStream( new FileInputStream( fileName ) ).readObject() );
-        System.out.println("\n\n");
+        smooksMain.runSmooksTransform(order);
+        System.out.println( LINE_SEP );
         pause("That's it ");
     }
 
@@ -89,9 +100,12 @@ public class Main
             BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
             System.out.print("> " + message);
             in.readLine();
-        } catch (IOException e) {
+        } 
+        catch (IOException e) 
+        {
+        	e.printStackTrace();
         }
-        System.out.println("\n");
+        System.out.println( LINE_SEP );
     }
 
 }
