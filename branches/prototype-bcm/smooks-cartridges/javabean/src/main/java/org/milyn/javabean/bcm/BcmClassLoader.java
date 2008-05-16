c62bd8acaf1968748a3f013517ee5133c076e77d
smies <smies@58448c06-5d2c-0410-b55c-e165fbcd3e60>
2008-05-16 11:46:39 +0000

    Classloader improvements

--- branches/prototype-bcm/smooks-cartridges/javabean/src/main/java/org/milyn/javabean/bcm/BcmClassLoader.java
+++ branches/prototype-bcm/smooks-cartridges/javabean/src/main/java/org/milyn/javabean/bcm/BcmClassLoader.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package org.milyn.javabean.bcm;
 
@@ -10,15 +10,15 @@ import org.milyn.Smooks;
 
 /**
  * @author <a href="mailto:maurice.zeijen@smies.com">maurice.zeijen@smies.com</a>
- * 
+ *
  */
 public class BcmClassLoader extends ClassLoader {
 
 	private final Map<String, Class<?>> loadedBCMClasses = new HashMap<String, Class<?>>();
-	
-	
+
+
 	/**
-	 * 
+	 *
 	 */
 	public BcmClassLoader() {
 		super(Smooks.class.getClassLoader());
@@ -30,18 +30,21 @@ public class BcmClassLoader extends ClassLoader {
 	public BcmClassLoader(ClassLoader parent) {
 		super(parent);
 	}
-	
+
 	public Class<?> load(String name) {
+		if(loadedBCMClasses.isEmpty()) {
+			return null;
+		}
 		return loadedBCMClasses.get(name);
 	}
 
 	public Class<?> load(String name, byte[] data) {
-	
+
 		Class<?> cls = super.defineClass(name, data, 0, data.length);
-		
+
 		loadedBCMClasses.put(name, cls);
-		
+
 		return cls;
-		
+
 	}
 }
