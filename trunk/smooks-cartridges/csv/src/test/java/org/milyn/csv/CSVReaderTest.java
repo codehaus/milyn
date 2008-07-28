1996c020b035f8d74cd1b2ac37ad07cfbd2931be
smies <smies@58448c06-5d2c-0410-b55c-e165fbcd3e60>
2008-07-28 19:41:35 +0000

    [MILYN-158] Create a CSVReader class that got all the logic of the CSVParser.  The CSVParser now extends the CSVReader and is now deprecated. Examples have been changed.

--- trunk/smooks-cartridges/csv/src/test/java/org/milyn/csv/CSVReaderTest.java
+++ trunk/smooks-cartridges/csv/src/test/java/org/milyn/csv/CSVReaderTest.java
@@ -3,52 +3,61 @@
 
 	This library is free software; you can redistribute it and/or
 	modify it under the terms of the GNU Lesser General Public
-	License (version 2.1) as published by the Free Software 
+	License (version 2.1) as published by the Free Software
 	Foundation.
 
 	This library is distributed in the hope that it will be useful,
 	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
-    
-	See the GNU Lesser General Public License for more details:    
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+	See the GNU Lesser General Public License for more details:
 	http://www.gnu.org/licenses/lgpl.txt
 */
 
 package org.milyn.csv;
 
 import java.io.ByteArrayInputStream;
-import java.io.UnsupportedEncodingException;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 
-import org.milyn.SmooksException;
-import org.milyn.Smooks;
-import org.milyn.SmooksUtil;
-import org.milyn.container.ExecutionContext;
-import org.milyn.profile.DefaultProfileSet;
-import org.milyn.cdr.SmooksResourceConfiguration;
-import org.milyn.xml.XmlUtil;
-import org.xml.sax.SAXException;
+import javax.xml.transform.dom.DOMResult;
+import javax.xml.transform.stream.StreamSource;
 
 import junit.framework.TestCase;
 
-import javax.xml.transform.stream.StreamSource;
-import javax.xml.transform.dom.DOMResult;
+import org.milyn.Smooks;
+import org.milyn.SmooksException;
+import org.milyn.SmooksUtil;
+import org.milyn.cdr.SmooksResourceConfiguration;
+import org.milyn.container.ExecutionContext;
+import org.milyn.profile.DefaultProfileSet;
+import org.milyn.xml.XmlUtil;
+import org.xml.sax.SAXException;
 
 /**
  * @author tfennelly
  */
-public class CSVParserTest extends TestCase {
+public class CSVReaderTest extends TestCase {
 
-	public void test_01() throws SmooksException, UnsupportedEncodingException {
+	public void test_01_csv_reader() throws SmooksException, UnsupportedEncodingException {
+		test_01(CSVReader.class);
+	}
+
+	@SuppressWarnings("deprecation")
+	public void test_01_csv_parser() throws SmooksException, UnsupportedEncodingException {
+		test_01(CSVParser.class);
+	}
+
+	public void test_01(Class<?> readerClass) throws SmooksException, UnsupportedEncodingException {
 		Smooks smooks = new Smooks();
 		SmooksResourceConfiguration config;
         ExecutionContext context;
 
-        config = new SmooksResourceConfiguration("org.xml.sax.driver", "type:Order-List and from:Acme", CSVParser.class.getName());
-		config.setParameter("fields", "string-list", "name,address");		
+        config = new SmooksResourceConfiguration("org.xml.sax.driver", "type:Order-List and from:Acme", readerClass.getName());
+		config.setParameter("fields", "string-list", "name,address");
 		SmooksUtil.registerResource(config, smooks);
 		SmooksUtil.registerProfileSet(DefaultProfileSet.create("Order-List-Acme-AcmePartner1", new String[] {"type:Order-List", "from:Acme", "to:AcmePartner1"}), smooks);
-		
+
 		String csvMessage;
         DOMResult domResult = new DOMResult();
 
@@ -65,12 +74,12 @@ public class CSVParserTest extends TestCase {
 		assertEquals("Ireland", XmlUtil.getString(domResult.getNode(), "/csv-set/csv-record[1]/address/text()"));
 		assertEquals("Joe Bloggs", XmlUtil.getString(domResult.getNode(), "/csv-set/csv-record[2]/name/text()"));
 		assertEquals("England", XmlUtil.getString(domResult.getNode(), "/csv-set/csv-record[2]/address/text()"));
-		
+
 		csvMessage = "Tom Fennelly\nJoe Bloggs,England";
         context = smooks.createExecutionContext("Order-List-Acme-AcmePartner1");
         smooks.filter(new StreamSource(new ByteArrayInputStream(csvMessage.getBytes("UTF-8"))), domResult, context);
 		assertEquals("Joe Bloggs", XmlUtil.getString(domResult.getNode(), "/csv-set/csv-record[1]/name/text()"));
-		assertEquals("England", XmlUtil.getString(domResult.getNode(), "/csv-set/csv-record[1]/address/text()"));		
+		assertEquals("England", XmlUtil.getString(domResult.getNode(), "/csv-set/csv-record[1]/address/text()"));
 	}
 
     public void test_02() throws SmooksException, IOException, SAXException {
@@ -92,4 +101,5 @@ public class CSVParserTest extends TestCase {
         String result = SmooksUtil.filterAndSerialize(context, getClass().getResourceAsStream("input-message-02.csv"), smooks);
         assertEquals("<csv-set><csv-record><firstname>Tom</firstname><lastname>Fennelly</lastname><gender>Male</gender><age>4</age><country>Ireland</country></csv-record><csv-record><firstname>Mike</firstname><lastname>Fennelly</lastname><gender>Male</gender><age>2</age><country>Ireland</country></csv-record></csv-set>", result);
     }
+
 }
