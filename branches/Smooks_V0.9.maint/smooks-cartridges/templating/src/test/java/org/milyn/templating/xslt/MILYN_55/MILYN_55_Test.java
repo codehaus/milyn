e86ed0dde094ce639e99713da78d92c68c48482a
tomfennelly <tomfennelly@58448c06-5d2c-0410-b55c-e165fbcd3e60>
2008-10-06 17:16:03 +0000

    removed silly try-catch

--- branches/Smooks_V0.9.maint/smooks-cartridges/templating/src/test/java/org/milyn/templating/xslt/MILYN_55/MILYN_55_Test.java
+++ branches/Smooks_V0.9.maint/smooks-cartridges/templating/src/test/java/org/milyn/templating/xslt/MILYN_55/MILYN_55_Test.java
@@ -31,22 +31,18 @@ import java.io.StringWriter;
 public class MILYN_55_Test extends TestCase {
 
     public void test() throws IOException, SAXException {
-        try {
-            Smooks smooks = new Smooks("/org/milyn/templating/xslt/MILYN_55/sample.xml");
-            StandaloneExecutionContext executionContext = smooks.createExecutionContext();
-            StringWriter resultWriter = new StringWriter();
+        Smooks smooks = new Smooks("/org/milyn/templating/xslt/MILYN_55/sample.xml");
+        StandaloneExecutionContext executionContext = smooks.createExecutionContext();
+        StringWriter resultWriter = new StringWriter();
 
-            smooks.filter(new StreamSource(getClass().getResourceAsStream("source.xml")), new StreamResult(resultWriter), executionContext);
-            assertEquals("<OrderLines>\n" +
-                    "\t\t<OrderLine position=\"1\" quantity=\"1\">\n" +
-                    "\t\t\t<Product price=\"29.98\" productId=\"364\" title=\"The 40-Year-Old Virgin \"></Product>\n" +
-                    "\t\t</OrderLine>\n" +
-                    "\t\t<OrderLine position=\"2\" quantity=\"1\">\n" +
-                    "\t\t\t<Product price=\"29.99\" productId=\"299\" title=\"Pulp Fiction\"></Product>\n" +
-                    "\t\t</OrderLine>\n" +
-                    "\t</OrderLines>", resultWriter.toString());
-        } catch (Throwable t) {
-            t.printStackTrace();
-        }
+        smooks.filter(new StreamSource(getClass().getResourceAsStream("source.xml")), new StreamResult(resultWriter), executionContext);
+        assertEquals("<OrderLines>\n" +
+                "\t\t<OrderLine position=\"1\" quantity=\"1\">\n" +
+                "\t\t\t<Product price=\"29.98\" productId=\"364\" title=\"The 40-Year-Old Virgin \"></Product>\n" +
+                "\t\t</OrderLine>\n" +
+                "\t\t<OrderLine position=\"2\" quantity=\"1\">\n" +
+                "\t\t\t<Product price=\"29.99\" productId=\"299\" title=\"Pulp Fiction\"></Product>\n" +
+                "\t\t</OrderLine>\n" +
+                "\t</OrderLines>", resultWriter.toString());
     }
 }
