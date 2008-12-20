39e468bdcca5a32a923f11e5b8faa4a83ef7662e
tomfennelly <tomfennelly@58448c06-5d2c-0410-b55c-e165fbcd3e60>
2008-12-20 09:34:25 +0000

    Added a test to make sure primitive lists are handled

---
+++ branches/Smooks_V1.0.maint/smooks-cartridges/javabean/src/test/java/org/milyn/javabean/java2java/A.java
@@ -0,0 +1,32 @@
+/*
+	Milyn - Copyright (C) 2006
+
+	This library is free software; you can redistribute it and/or
+	modify it under the terms of the GNU Lesser General Public
+	License (version 2.1) as published by the Free Software
+	Foundation.
+
+	This library is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+	See the GNU Lesser General Public License for more details:
+	http://www.gnu.org/licenses/lgpl.txt
+*/
+package org.milyn.javabean.java2java;
+
+/**
+ * @author <a href="mailto:tom.fennelly@gmail.com">tom.fennelly@gmail.com</a>
+ */
+public class A {
+
+    private String[] aStrings = new String[] {"1", "2", "3"};
+
+    public String[] getAStrings() {
+        return aStrings;
+    }
+
+    public void setAStrings(String[] aStrings) {
+        this.aStrings = aStrings;
+    }
+}
