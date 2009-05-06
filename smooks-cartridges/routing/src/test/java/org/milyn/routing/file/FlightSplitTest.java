e7cffe063b69cf407fb8bbb576b241e4f63c4aa1
tomfennelly <tomfennelly@58448c06-5d2c-0410-b55c-e165fbcd3e60>
2009-05-06 17:31:12 +0000

    http://jira.codehaus.org/browse/MILYN-295 Finished test

--- smooks-cartridges/routing/src/test/java/org/milyn/routing/file/FlightSplitTest.java
+++ smooks-cartridges/routing/src/test/java/org/milyn/routing/file/FlightSplitTest.java
@@ -18,18 +18,47 @@ package org.milyn.routing.file;
 import junit.framework.TestCase;
 import org.milyn.Smooks;
 import org.xml.sax.SAXException;
+import org.custommonkey.xmlunit.XMLUnit;
 
 import javax.xml.transform.stream.StreamSource;
 import java.io.IOException;
+import java.io.File;
+import java.io.FileReader;
+import java.io.InputStreamReader;
 
 /**
  * @author <a href="mailto:tom.fennelly@jboss.com">tom.fennelly@jboss.com</a>
  */
 public class FlightSplitTest extends TestCase {
 
+    protected void setUp() throws Exception {
+        new File("target/flights/BA-1234.xml").delete();
+        new File("target/flights/BA-5678.xml").delete();
+    }
+
     public void test() throws IOException, SAXException {
         Smooks smooks = new Smooks(getClass().getResourceAsStream("flight-split.xml"));
 
-        smooks.filter(new StreamSource(getClass().getResourceAsStream("flight.xml")));
+        try {
+            smooks.filter(new StreamSource(getClass().getResourceAsStream("flight.xml")));
+        } finally {
+            smooks.close();
+        }
+
+        XMLUnit.setIgnoreWhitespace(true);
+
+        FileReader fileReader = new FileReader(new File("target/flights/BA-1234.xml"));
+        try {
+            assertTrue(XMLUnit.compareXML(fileReader, new InputStreamReader(getClass().getResourceAsStream("BA-1234.xml"))).identical());
+        } finally {
+            fileReader.close();
+        }
+
+        fileReader = new FileReader(new File("target/flights/BA-5678.xml"));
+        try {
+            assertTrue(XMLUnit.compareXML(fileReader, new InputStreamReader(getClass().getResourceAsStream("BA-5678.xml"))).identical());
+        } finally {
+            fileReader.close();
+        }
     }
 }
