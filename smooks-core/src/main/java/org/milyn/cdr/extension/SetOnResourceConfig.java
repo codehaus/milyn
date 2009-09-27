c10aeff7a322bbf899de2f3f6abe52ffd8be6c63
tomfennelly <tomfennelly@58448c06-5d2c-0410-b55c-e165fbcd3e60>
2009-09-27 18:22:19 +0000

    http://jira.codehaus.org/browse/MILYN-378 Add a "core" or "runtime" configuration namespace for general runtime configurations

--- smooks-core/src/main/java/org/milyn/cdr/extension/SetOnResourceConfig.java
+++ smooks-core/src/main/java/org/milyn/cdr/extension/SetOnResourceConfig.java
@@ -18,7 +18,6 @@ package org.milyn.cdr.extension;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.milyn.SmooksException;
-import org.milyn.xml.DomUtils;
 import org.milyn.cdr.SmooksResourceConfiguration;
 import org.milyn.cdr.annotation.ConfigParam;
 import org.milyn.container.ExecutionContext;
