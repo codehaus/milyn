8a4ae133a10057316316baeb2d1ca30a3e53691e
smies <smies@58448c06-5d2c-0410-b55c-e165fbcd3e60>
2009-10-26 20:19:24 +0000

    Worked on MILYN-368

--- trunk/commons/src/main/java/org/milyn/function/StringFunctionDefinitionParser.java
+++ trunk/commons/src/main/java/org/milyn/function/StringFunctionDefinitionParser.java
@@ -36,31 +36,44 @@ public class StringFunctionDefinitionParser {
     public static final CapitalizeFirstFunction CAPITALIZE_FIRST_FUNCTION = new CapitalizeFirstFunction();
     public static final UncapitalizeFirstFunction UNCAPITALIZE_FIRST_FUNCTION = new UncapitalizeFirstFunction();
 
+    public static final String TRIM_DEFINITION = "trim";
+    public static final String LEFT_TRIM_DEFINITION = "left_trim";
+    public static final String RIGHT_TRIM_DEFINITION = "right_trim";
+    public static final String UPPER_CASE_DEFINITION = "upper_case";
+    public static final String LOWER_CASE_DEFINITION = "lower_case";
+    public static final String CAPITALIZE_DEFINITION = "capitalize";
+    public static final String CAPITALIZE_FIRST_DEFINITION = "cap_first";
+    public static final String UNCAPITALIZE_FIRST_DEFINITION = "uncap_first";
+    
+    public static final char SEPARATOR = '.';
+
     private StringFunctionDefinitionParser() {
     }
 
     public static List<StringFunction> parse(String definition) {
         List<StringFunction> functions = new ArrayList<StringFunction>();
 
-        String[] functionsDef = StringUtils.split(definition, '.');
+        String[] functionsDef = StringUtils.split(definition, SEPARATOR);
 
         for(String functionDef : functionsDef) {
-            if(functionDef.equals("trim")) {
+            if(functionDef.equals(TRIM_DEFINITION)) {
                 functions.add(TRIM_FUNCTION);
-            }else if(functionDef.equals("ltrim")) {
+            }else if(functionDef.equals(LEFT_TRIM_DEFINITION)) {
                 functions.add(LEFT_TRIM_FUNCTION);
-            }else if(functionDef.equals("rtrim")) {
+            }else if(functionDef.equals(RIGHT_TRIM_DEFINITION)) {
                 functions.add(RIGHT_TRIM_FUNCTION);
-            } else if(functionDef.equals("upper_case")) {
+            } else if(functionDef.equals(UPPER_CASE_DEFINITION)) {
                 functions.add(UPPER_CASE_FUNCTION);
-            } else if(functionDef.equals("lower_case")) {
+            } else if(functionDef.equals(LOWER_CASE_DEFINITION)) {
                 functions.add(LOWER_CASE_FUNCTION);
-            }  else if(functionDef.equals("capitalize")) {
+            }  else if(functionDef.equals(CAPITALIZE_DEFINITION)) {
                 functions.add(CAPITALIZE_FUNCTION);
-            }  else if(functionDef.equals("cap_first")) {
+            }  else if(functionDef.equals(CAPITALIZE_FIRST_DEFINITION)) {
                 functions.add(CAPITALIZE_FIRST_FUNCTION);
-            }  else if(functionDef.equals("uncap_first")) {
+            }  else if(functionDef.equals(UNCAPITALIZE_FIRST_DEFINITION)) {
                 functions.add(UNCAPITALIZE_FIRST_FUNCTION);
+            }  else {
+                throw new UnknownStringFunctionException("The function '"+ functionDef +"' in the function definition '"+ definition +"' is unknown.");
             }
         }
 
