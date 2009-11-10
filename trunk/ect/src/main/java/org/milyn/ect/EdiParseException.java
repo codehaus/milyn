78621c3a5b46835fb294dd84cc99d19e9a1150f8
bardl <bardl@58448c06-5d2c-0410-b55c-e165fbcd3e60>
2009-11-10 19:29:16 +0000

    http://jira.codehaus.org/browse/MILYN-397

---
+++ trunk/ect/src/main/java/org/milyn/ect/EdiParseException.java
@@ -0,0 +1,27 @@
+package org.milyn.ect;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: bardl
+ * Date: 2009-okt-14
+ * Time: 07:46:26
+ * To change this template use File | Settings | File Templates.
+ */
+public class EdiParseException extends Exception {
+
+    public EdiParseException() {
+    }
+
+    public EdiParseException(String message) {
+        super(message);
+    }
+
+    public EdiParseException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public EdiParseException(Throwable cause) {
+        super(cause);
+    }
+
+}
