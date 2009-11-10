c6ce65878a669e48024c77f64f7fa3a4cccbf9ea
smies <smies@58448c06-5d2c-0410-b55c-e165fbcd3e60>
2009-11-10 09:06:43 +0000

    Work on MILYN-349

--- trunk/smooks-cartridges/javabean/src/main/java/org/milyn/javabean/Value.java
+++ trunk/smooks-cartridges/javabean/src/main/java/org/milyn/javabean/Value.java
@@ -1,3 +1,18 @@
+/*
+	Milyn - Copyright (C) 2006
+
+	This library is free software; you can redistribute it and/or
+	modify it under the terms of the GNU Lesser General Public
+	License (version 2.1) as published by the Free Software
+	Foundation.
+
+	This library is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+	See the GNU Lesser General Public License for more details:
+	http://www.gnu.org/licenses/lgpl.txt
+*/
 package org.milyn.javabean;
 
 import org.milyn.assertion.AssertArgument;
@@ -7,9 +22,55 @@ import org.milyn.delivery.VisitorConfigMap;
 import org.milyn.javabean.ext.SelectorPropertyResolver;
 
 /**
+ * Programmatic Value Configurator.
+ * <p/>
+ * This class can be used to programmatically configure a Smooks instance for creating value
+ * objects using the Smooks DataDecoders.
+ * <p/>
+ * This class uses a Fluent API (all methods return the Bean instance), making it easy to
+ * string configurations together.
+ * <p/>
+ * <h3>Example</h3>
+ * Taking the "classic" Order message as an example and getting the order number and
+ * name as Value Objects in the form of an Integer and String.
+ * <h4>The Message</h4>
+ * <pre>
+ * &lt;order xmlns="http://x"&gt;
+ *     &lt;header&gt;
+ *         &lt;y:date xmlns:y="http://y"&gt;Wed Nov 15 13:45:28 EST 2006&lt;/y:date&gt;
+ *         &lt;customer number="123123"&gt;Joe&lt;/customer&gt;
+ *         &lt;privatePerson&gt;&lt;/privatePerson&gt;
+ *     &lt;/header&gt;
+ *     &lt;order-items&gt;
+ *         &lt;!-- .... --!&gt;
+ *     &lt;/order-items&gt;
+ * &lt;/order&gt;
+ * </pre>
+ * <p/>
+ * <h4>The Binding Configuration and Execution Code</h4>
+ * The configuration code (Note: Smooks instance defined and instantiated globally):
+ * <pre>
+ * Smooks smooks = new Smooks();
  *
- * @author maurice_zeijen
+ * Value customerNumberValue = new Value( "customerNumber", "customer/@number")
+ *                                   .setDecoder("Integer");
+ * Value customerNameValue = new Value( "customerName", "customer")
+ *                                   .setDefault("Unknown");
  *
+ * smooks.addVisitors(customerNumberValue);
+ * smooks.addVisitors(customerNameValue);
+ * </pre>
+ * <p/>
+ * And the execution code:
+ * <pre>
+ * JavaResult result = new JavaResult();
+ *
+ * smooks.filterSource(new StreamSource(orderMessageStream), result);
+ * Integer customerNumber = (Integer) result.getBean("customerNumber");
+ * String customerName = (String) result.getBean("customerName");
+ * </pre>
+ *
+ * @author <a href="mailto:maurice.zeijen@smies.com">maurice.zeijen@smies.com</a>
  */
 public class Value implements VisitorAppender {
 
@@ -23,6 +84,12 @@ public class Value implements VisitorAppender {
 
 	private DataDecoder decoder;
 
+	/**
+     * Create a Value binding configuration.
+     *
+	 * @param beanId The bean id under which the value will be stored.
+	 * @param data The data selector for the data value to be bound.
+	 */
 	public Value(String beanId, String data) {
 		AssertArgument.isNotNullAndNotEmpty(beanId, "beanId");
 		AssertArgument.isNotNullAndNotEmpty(data, "dataSelector");
@@ -31,25 +98,46 @@ public class Value implements VisitorAppender {
 		this.dataSelector = data;
 	}
 
+	/**
+	 * The namespace for the data selector for the data value to be bound.
+	 *
+	 * @param targetNamespace The namespace
+	 * @return <code>this</code> Value configuration instance.
+	 */
 	public Value setTargetNamespace(String targetNamespace) {
 		this.targetNamespace = targetNamespace;
 
 		return this;
 	}
 
+	/**
+	 * The default value for if the data is null or empty
+	 *
+	 * @param targetNamespace The default value
+	 * @return <code>this</code> Value configuration instance.
+	 */
 	public Value setDefaultValue(String defaultValue) {
 		this.defaultValue = defaultValue;
 
 		return this;
 	}
 
-
+	/**
+	 * The {@link org.milyn.javabean.DataDecoder} to be used for decoding
+     * the data value.
+	 *
+	 * @param targetNamespace The {@link org.milyn.javabean.DataDecoder}
+	 * @return <code>this</code> Value configuration instance.
+	 */
 	public Value setDecoder(DataDecoder dataDecoder) {
 		this.decoder = dataDecoder;
 
 		return this;
 	}
 
+	/**
+	 * Used by Smooks to retrieve the visitor configuration of this Value Configuration
+	 */
 	public void addVisitors(VisitorConfigMap visitorMap) {
 
 		ValueBinder binder = new ValueBinder(beanId);
