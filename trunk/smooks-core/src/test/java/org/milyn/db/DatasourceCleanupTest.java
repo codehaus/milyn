79af7b2f08ea03a793460f5afdd36ac68cbff540
smies <smies@58448c06-5d2c-0410-b55c-e165fbcd3e60>
2009-11-19 07:35:50 +0000

    Housekeeping

--- trunk/smooks-core/src/test/java/org/milyn/db/DatasourceCleanupTest.java
+++ trunk/smooks-core/src/test/java/org/milyn/db/DatasourceCleanupTest.java
@@ -37,7 +37,7 @@ public class DatasourceCleanupTest extends TestCase {
 
         // Cleanup should get called twice.  Once for the visitAfter event and once for the
         // executeExecutionLifecycleCleanup event...
-        smooks.filterSource(new StringSource("<a></a>"), null);
+        smooks.filterSource(new StringSource("<a></a>"));
         assertEquals(2, MockDatasource.cleanupCallCount);
         assertTrue(MockDatasource.committed);
     }
@@ -46,7 +46,7 @@ public class DatasourceCleanupTest extends TestCase {
         Smooks smooks = new Smooks(getClass().getResourceAsStream("exception-ds-lifecycle.xml"));
 
         try {
-            smooks.filterSource(new StringSource("<a><b/><c/></a>"), null);
+            smooks.filterSource(new StringSource("<a><b/><c/></a>"));
             fail("Expected exception...");
         } catch(SmooksException e) {
             // Expected
