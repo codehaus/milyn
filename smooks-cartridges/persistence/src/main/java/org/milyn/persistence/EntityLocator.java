da5702b16cff0503a15ae4448ae7d7a8ae62bb98
smies <smies@58448c06-5d2c-0410-b55c-e165fbcd3e60>
2009-12-08 08:00:00 +0000

    Fixed MILYN-402 for maintenance branch

--- smooks-cartridges/persistence/src/main/java/org/milyn/persistence/EntityLocator.java
+++ smooks-cartridges/persistence/src/main/java/org/milyn/persistence/EntityLocator.java
@@ -17,7 +17,7 @@ package org.milyn.persistence;
 
 import java.io.IOException;
 import java.util.Collection;
-import java.util.Collections;
+import java.util.;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -183,18 +183,30 @@ public class EntityLocator implements DOMElementVisitor, SAXVisitBefore, SAXVisi
 
 			Object result = lookup(dao, executionContext);
 
-			if(uniqueResult == true) {
+			if(result != null && uniqueResult == true) {
 				if(result instanceof Collection){
 					Collection<Object> resultCollection = (Collection<Object>) result;
 
-					if(resultCollection.size() == 1) {
+					if(resultCollection.size() == 0) {
+						result = null;
+					} else if(resultCollection.size() == 1) {
 						for(Object value : resultCollection) {
 							result = value;
 						}
-					} else if(resultCollection.size() == 1) {
-						result = null;
 					} else {
-						throw new NonUniqueResultException("The DAO '" + daoName + "' returned multiple results for the lookup '" + lookupName + "'");
+						String exception;
+						if(daoName == null) {
+							exception = "The " + getDaoNameFromAdapter(dao) + " DAO";
+						} else {
+							exception = "The DAO '" + daoName + "'";
+						}
+						exception += " returned multiple results for the ";
+						if(lookupName != null) {
+							exception += "lookup '" + lookupName + "'";
+						} else {
+							exception += "query '" + query + "'";
+						}
+						throw new NonUniqueResultException(exception);
 					}
 
 				} else {
@@ -204,11 +216,19 @@ public class EntityLocator implements DOMElementVisitor, SAXVisitBefore, SAXVisi
 			}
 
 			if(result == null && onNoResult == OnNoResult.EXCEPTION) {
-				if(lookupName != null) {
-					throw new NoLookupResultException("The DAO '" + daoName + "' returned no results for lookup '" + lookupName + "'");
+				String exception;
+				if(daoName == null) {
+					exception = "The " + getDaoNameFromAdapter(dao) + " DAO";
 				} else {
-					throw new NoLookupResultException("The DAO '" + daoName + "' returned no results found for query '" + query + "'");
+					exception = "The DAO '" + daoName + "'";
 				}
+				exception += " returned no results for lookup ";
+				if(lookupName != null) {
+					exception += "lookup '" + query + "'";
+				} else {
+					exception += "query '" + query + "'";
+				}
+				throw new NoLookupResultException(exception);
 			}
 
 			BeanRepository beanRepository = BeanRepository.getInstance(executionContext);
