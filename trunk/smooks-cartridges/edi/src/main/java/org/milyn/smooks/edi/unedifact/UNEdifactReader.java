947b87b81b44593614d4265d5e893fcf6ef90992
tomfennelly <tomfennelly@58448c06-5d2c-0410-b55c-e165fbcd3e60>
2010-03-10 20:55:41 +0000

    http://jira.codehaus.org/browse/MILYN-439 Dynamic Model Builder

---
+++ trunk/smooks-cartridges/edi/src/main/java/org/milyn/smooks/edi/unedifact/UNEdifactReader.java
@@ -0,0 +1,35 @@
+/*
+	Milyn - Copyright (C) 2006 - 2010
+
+	This library is free software; you can redistribute it and/or
+	modify it under the terms of the GNU Lesser General Public
+	License (version 2.1) as published by the Free Software
+	Foundation.
+
+	This library is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+	See the GNU Lesser General Public License for more details:
+	http://www.gnu.org/licenses/lgpl.txt
+*/
+package org.milyn.smooks.edi.unedifact;
+
+import org.milyn.container.ExecutionContext;
+import org.milyn.edisax.unedifact.InterchangeParser;
+import org.milyn.xml.SmooksXMLReader;
+
+/**
+ * UN/EDIFACT Smooks reader.
+ * 
+ * @author <a href="mailto:tom.fennelly@gmail.com">tom.fennelly@gmail.com</a>
+ */
+public class UNEdifactReader extends InterchangeParser implements SmooksXMLReader  {
+
+	/* (non-Javadoc)
+	 * @see org.milyn.xml.SmooksXMLReader#setExecutionContext(org.milyn.container.ExecutionContext)
+	 */
+	public void setExecutionContext(ExecutionContext executionContext) {
+		
+	}
+}
