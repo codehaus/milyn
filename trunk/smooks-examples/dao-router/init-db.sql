79077c0f151cf9aa7dae6f00457c402dcfa615a2
smies <smies@58448c06-5d2c-0410-b55c-e165fbcd3e60>
2010-08-10 08:56:51 +0000

    Added iBatis to the DAO router example

--- trunk/smooks-examples/dao-router/init-db.sql
+++ trunk/smooks-examples/dao-router/init-db.sql
@@ -5,6 +5,10 @@ delete from products;
 
 insert into customers (id, name) values (123456, 'Devin Snow');
 insert into customers (id, name) values (789101, 'Homer Simpson');
-insert into products  (id, name) values (11, 'cheese cake');
-insert into products  (id, name) values (22, 'chocolate');
-insert into products  (id, name) values (33, 'beer');
+insert into customers (id, name) values (999999, 'John Doe');
+
+insert into products  (id, name) values (11, 'Cheese cake');
+insert into products  (id, name) values (22, 'Chocolate 300gr');
+insert into products  (id, name) values (33, 'Beer');
+insert into products  (id, name) values (44, 'Smooks in Action');
+insert into products  (id, name) values (55, 'Ultimate guide to Smooks');
