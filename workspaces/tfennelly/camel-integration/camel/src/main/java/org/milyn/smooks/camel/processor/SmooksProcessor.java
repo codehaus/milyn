f5c7259a561ea3cf213bbc0b579b107b1265b66a
danielbevenius <danielbevenius@58448c06-5d2c-0410-b55c-e165fbcd3e60>
2010-08-25 07:24:08 +0000

    Reformatted the class.

--- workspaces/tfennelly/camel-integration/camel/src/main/java/org/milyn/smooks/camel/processor/SmooksProcessor.java
+++ workspaces/tfennelly/camel-integration/camel/src/main/java/org/milyn/smooks/camel/processor/SmooksProcessor.java
@@ -46,167 +46,164 @@ import org.xml.sax.SAXException;
  */
 public class SmooksProcessor implements Processor, Service
 {
-	public static final String SMOOKS_EXECUTION_CONTEXT = "CamelSmooksExecutionContext";
-	
-    private final Log log = LogFactory.getLog(getClass());
-	private Smooks smooks;
-	private String configUri;
-	private String reportPath;
-	private Result result;
-	
-	private Set<VisitorAppender> visitorAppenders = new HashSet<VisitorAppender>();
-	private Map<String, Visitor> selectorVisitorMap = new HashMap<String, Visitor>();
+    public static final String SMOOKS_EXECUTION_CONTEXT = "CamelSmooksExecutionContext";
+    private final Log log = LogFactory.getLog(SmooksProcessor.class);
+    private Smooks smooks;
+    private String configUri;
+    private String reportPath;
+    private Result result;
 
-	public SmooksProcessor(Smooks smooks)
-	{
-		this.smooks = smooks;
-	}
+    private Set<VisitorAppender> visitorAppenders = new HashSet<VisitorAppender>();
+    private Map<String, Visitor> selectorVisitorMap = new HashMap<String, Visitor>();
 
-	public SmooksProcessor(String configUri) throws IOException, SAXException
-	{
-		this.configUri = configUri;
-	}
+    public SmooksProcessor(Smooks smooks)
+    {
+        this.smooks = smooks;
+    }
 
-	public void process(Exchange exchange) throws Exception
-	{
-		ExecutionContext executionContext = smooks.createExecutionContext();
-		executionContext.setAttribute(Exchange.class, exchange);
-		exchange.getOut().setHeader(SMOOKS_EXECUTION_CONTEXT, executionContext);
-		setupSmooksReporting(executionContext);
-		
-		if (result != null)
-		{
-			Source source = getSource(exchange);
-			smooks.filterSource(executionContext, source, result);
-			exchange.getOut().setBody(result);
-		}
-		else
-		{
-			Source source = getSource(exchange);
-			smooks.filterSource(executionContext, source);
-		}
-		executionContext.removeAttribute(Exchange.class);
-	}
+    public SmooksProcessor(String configUri) throws IOException, SAXException
+    {
+        this.configUri = configUri;
+    }
 
-	private void setupSmooksReporting(ExecutionContext executionContext)
-	{
-		if (reportPath != null)
-		{
-			try
-			{
-				executionContext.setEventListener(new HtmlReportGenerator(reportPath));
-			} 
-			catch (IOException e)
-			{
-				log.info("Could not generate Smooks Report. The reportPath specified was [" + reportPath + "].", e);
-			}
-		}
-	}
+    public void process(Exchange exchange) throws Exception
+    {
+        ExecutionContext executionContext = smooks.createExecutionContext();
+        executionContext.setAttribute(Exchange.class, exchange);
+        exchange.getOut().setHeader(SMOOKS_EXECUTION_CONTEXT, executionContext);
+        setupSmooksReporting(executionContext);
 
-	private Source getSource(Exchange exchange)
-	{
-		return exchange.getIn().getBody(Source.class);
-	}
-	
-	public String getSmooksConfig()
-	{
-		return configUri;
-	}
+        if (result != null)
+        {
+            Source source = getSource(exchange);
+            smooks.filterSource(executionContext, source, result);
+            exchange.getOut().setBody(result);
+        } else
+        {
+            Source source = getSource(exchange);
+            smooks.filterSource(executionContext, source);
+        }
+        executionContext.removeAttribute(Exchange.class);
+    }
 
-	public void setSmooksConfig(String smooksConfig)
-	{
-		this.configUri = smooksConfig;
-	}
+    private void setupSmooksReporting(ExecutionContext executionContext)
+    {
+        if (reportPath != null)
+        {
+            try
+            {
+                executionContext.setEventListener(new HtmlReportGenerator(reportPath));
+            } catch (IOException e)
+            {
+                log.info("Could not generate Smooks Report. The reportPath specified was [" + reportPath + "].", e);
+            }
+        }
+    }
 
-	public SmooksProcessor setResultType(String resultType)
-	{
-		if (resultType != null)
-		{
-			Class<?> loadClass = ObjectHelper.loadClass(resultType);
-			this.result = (Result) ObjectHelper.newInstance(loadClass);
-		}
-		return this;
-	}
-	
-	/**
-	 * Add a visitor instance.
-	 * 
-	 * @param visitor
-	 *            The visitor implementation.
-	 * @param targetSelector
-	 *            The message fragment target selector.
-	 * @return This instance.
-	 */
-	public SmooksProcessor addVisitor(Visitor visitor, String targetSelector)
-	{
-		selectorVisitorMap.put(targetSelector, visitor);
-		return this;
-	}
+    private Source getSource(Exchange exchange)
+    {
+        return exchange.getIn().getBody(Source.class);
+    }
 
-	/**
-	 * Add a visitor instance to <code>this</code> Smooks instance via a
-	 * {@link VisitorAppender}.
-	 * 
-	 * @param appender
-	 *            The visitor appender.
-	 * @return This instance.
-	 */
-	public SmooksProcessor addVisitor(VisitorAppender appender)
-	{
-		visitorAppenders.add(appender);
-		return this;
-	}
+    public String getSmooksConfig()
+    {
+        return configUri;
+    }
 
-	public void setReportPath(String reportPath)
-	{
-		this.reportPath = reportPath;
-	}
+    public void setSmooksConfig(String smooksConfig)
+    {
+        this.configUri = smooksConfig;
+    }
 
-	public void start() throws Exception
-	{
-		if (smooks == null)
-		{
-			smooks = createSmooks(configUri);
-		}
-		addAppenders(smooks, visitorAppenders);
-		addVisitors(smooks, selectorVisitorMap);
-		log.info(this + " Started");
-	}
-	
-	private Smooks createSmooks(String configUri) throws IOException, SAXException
-	{
-		if (smooks != null)
-			return smooks;
-		
-		return new Smooks(configUri);
-	}
-	
-	private void addAppenders(Smooks smooks, Set<VisitorAppender> appenders)
-	{
-		for (VisitorAppender appender : visitorAppenders)
-			smooks.addVisitor(appender);
-	}
-	
-	private void addVisitors(Smooks smooks, Map<String, Visitor> selectorVisitorMap)
-	{
-		for (Entry<String, Visitor> entry : selectorVisitorMap.entrySet())
-			smooks.addVisitor(entry.getValue(), entry.getKey());
-	}
+    public SmooksProcessor setResultType(String resultType)
+    {
+        if (resultType != null)
+        {
+            Class<?> loadClass = ObjectHelper.loadClass(resultType);
+            this.result = (Result) ObjectHelper.newInstance(loadClass);
+        }
+        return this;
+    }
 
-	public void stop() throws Exception
-	{
-		if (smooks != null)
-		{
-			smooks.close();
-			smooks = null;
-		}
-		log.info(this + " Stopped");
-	}
-	
-	@Override
-	public String toString()
-	{
-		return "SmooksProcessor [configUri=" + configUri + "]";
-	}
+    /**
+     * Add a visitor instance.
+     * 
+     * @param visitor
+     *            The visitor implementation.
+     * @param targetSelector
+     *            The message fragment target selector.
+     * @return This instance.
+     */
+    public SmooksProcessor addVisitor(Visitor visitor, String targetSelector)
+    {
+        selectorVisitorMap.put(targetSelector, visitor);
+        return this;
+    }
+
+    /**
+     * Add a visitor instance to <code>this</code> Smooks instance via a
+     * {@link VisitorAppender}.
+     * 
+     * @param appender
+     *            The visitor appender.
+     * @return This instance.
+     */
+    public SmooksProcessor addVisitor(VisitorAppender appender)
+    {
+        visitorAppenders.add(appender);
+        return this;
+    }
+
+    public void setReportPath(String reportPath)
+    {
+        this.reportPath = reportPath;
+    }
+
+    public void start() throws Exception
+    {
+        if (smooks == null)
+        {
+            smooks = createSmooks(configUri);
+        }
+        addAppenders(smooks, visitorAppenders);
+        addVisitors(smooks, selectorVisitorMap);
+        log.info(this + " Started");
+    }
+
+    private Smooks createSmooks(String configUri) throws IOException, SAXException
+    {
+        if (smooks != null)
+            return smooks;
+
+        return new Smooks(configUri);
+    }
+
+    private void addAppenders(Smooks smooks, Set<VisitorAppender> appenders)
+    {
+        for (VisitorAppender appender : visitorAppenders)
+            smooks.addVisitor(appender);
+    }
+
+    private void addVisitors(Smooks smooks, Map<String, Visitor> selectorVisitorMap)
+    {
+        for (Entry<String, Visitor> entry : selectorVisitorMap.entrySet())
+            smooks.addVisitor(entry.getValue(), entry.getKey());
+    }
+
+    public void stop() throws Exception
+    {
+        if (smooks != null)
+        {
+            smooks.close();
+            smooks = null;
+        }
+        log.info(this + " Stopped");
+    }
+
+    @Override
+    public String toString()
+    {
+        return "SmooksProcessor [configUri=" + configUri + "]";
+    }
 
 }
