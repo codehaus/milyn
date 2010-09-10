2164414e6bdc867feafb5a608d6b8042c7284e14
zubairov <zubairov@58448c06-5d2c-0410-b55c-e165fbcd3e60>
2010-09-10 10:51:24 +0000

    Updated XML Schema for UN/EDIFACT Envelope

--- workspaces/zubairov/edi/ecore/src/main/java/org/mylin/ecore/resource/EDIPackageRegistry.java
+++ workspaces/zubairov/edi/ecore/src/main/java/org/mylin/ecore/resource/EDIPackageRegistry.java
@@ -11,15 +11,15 @@ import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.emf.ecore.xmi.impl.EcoreResourceFactoryImpl;
+import org.milyn.edisax.interchange.ControlBlockHandler;
 import org.milyn.edisax.model.EdifactModel;
-import org.milyn.edisax.model.internal.IEdimap;
 import org.milyn.edisax.unedifact.UNEdifactInterchangeParser.MappingRegistry;
 import org.mylin.ecore.model.EdimapAdapter;
 import org.xml.sax.SAXException;
 
 /**
- * A ultimate registry that serves to EMF loading framework as well as
- * to UN EDIFACT parser.
+ * A ultimate registry that serves to EMF loading framework as well as to UN
+ * EDIFACT parser.
  * 
  * Loading models on demand
  * 
@@ -33,48 +33,62 @@ public class EDIPackageRegistry extends EPackageRegistryImpl implements
 	 * UID
 	 */
 	private static final long serialVersionUID = -400421356940005210L;
+	private EPackage envelope;
+	private EPackage cuscar;
 
 	public EDIPackageRegistry() {
 		super(EPackage.Registry.INSTANCE);
+		try {
+			envelope = loadModel("model/envelope.ecore");
+			cuscar = loadModel("cuscar.ecore");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public EdifactModel getModel(String lookupName) throws SAXException {
-		EPackage pkg;
-		try {
-			pkg = loadCUSCARModel();
-			IEdimap edimap = new EdimapAdapter(pkg);
-			return new EdifactModel(edimap);
-		} catch (IOException e) {
-			throw new SAXException("Can't load model", e);
-		}
+		return new EdifactModel(new EdimapAdapter(cuscar));
 	}
 
 	/**
 	 * Loading CUSCAR ecore model
 	 * 
+	 * @param modelName
+	 *            TODO
+	 * 
 	 * @return
 	 * @throws IOException
 	 */
-	private EPackage loadCUSCARModel() throws IOException {
+	private EPackage loadModel(String modelName) throws IOException {
 		ResourceSet rs = new ResourceSetImpl();
 		rs.getResourceFactoryRegistry()
 				.getExtensionToFactoryMap()
 				.put(Resource.Factory.Registry.DEFAULT_EXTENSION,
 						new EcoreResourceFactoryImpl());
-		Resource resource = rs
-				.createResource(URI.createFileURI("cuscar.ecore"));
-		resource.load(null);
+		Resource resource = rs.getResource(URI.createFileURI(modelName), true);
 		EPackage pkg = (EPackage) resource.getAllContents().next();
 		return pkg;
 	}
-	
+
 	@Override
 	public EPackage getEPackage(String nsURI) {
+		if (ControlBlockHandler.NAMESPACE.equals(nsURI)) {
+			return envelope;
+		}
+		if (nsURI != null && nsURI.endsWith("CUSCAR")) {
+			return cuscar;
+		}
 		return super.getEPackage(nsURI);
 	}
-	
+
 	@Override
 	public EFactory getEFactory(String nsURI) {
+		if (ControlBlockHandler.NAMESPACE.equals(nsURI)) {
+			return envelope.getEFactoryInstance();
+		}
+		if (nsURI != null && nsURI.endsWith("CUSCAR")) {
+			return cuscar.getEFactoryInstance();
+		}
 		return super.getEFactory(nsURI);
 	}
 
--- workspaces/zubairov/edi/ecore/src/test/java/org/mylin/ecore/EDIResourceSetTest.java
+++ workspaces/zubairov/edi/ecore/src/test/java/org/mylin/ecore/EDIResourceSetTest.java
@@ -1,22 +1,35 @@
 package org.mylin.ecore;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import junit.framework.TestCase;
 
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.xmi.XMLResource;
+import org.eclipse.emf.ecore.xmi.impl.XMLResourceFactoryImpl;
 import org.mylin.ecore.resource.EDIFactResourceFactoryImpl;
 import org.mylin.ecore.resource.EDIPackageRegistry;
 
 public class EDIResourceSetTest extends TestCase {
 
 	public void testEDILoading() throws Exception {
+		//String fileName = "/99a_cuscar_out.xml";
+		String fileName = "/99a_cuscar.edi";
 		EDIPackageRegistry registry = new EDIPackageRegistry();
 		ResourceSetImpl rs = new ResourceSetImpl();
 		rs.getResourceFactoryRegistry().getExtensionToFactoryMap()
-				.put("*", new EDIFactResourceFactoryImpl(registry));
-		Resource resource = rs.createResource(URI.createURI("test.edi"));
-		resource.load(getClass().getResourceAsStream("/99a_cuscar.edi"), null);
+				.put("edi", new EDIFactResourceFactoryImpl(registry));
+		rs.getResourceFactoryRegistry().getExtensionToFactoryMap()
+				.put("xml", new XMLResourceFactoryImpl());
+		rs.setPackageRegistry(registry);
+		Resource resource = rs.createResource(URI.createURI(fileName));
+		Map<String, Boolean> map = new HashMap<String, Boolean>();
+		map.put(XMLResource.OPTION_EXTENDED_META_DATA, true);
+		resource.load(getClass().getResourceAsStream(fileName),
+				map);
 	}
 
 }
