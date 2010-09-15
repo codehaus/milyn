0fcfabab13859769923e55361acbbf1169469fe3
zubairov <zubairov@58448c06-5d2c-0410-b55c-e165fbcd3e60>
2010-09-15 14:52:16 +0000

    Added project for generic EDI Editor

---
+++ workspaces/zubairov/org.smooks.edi.editor/src/org/smooks/edi/editor/EDIEditor.java
@@ -0,0 +1,22 @@
+package org.smooks.edi.editor;
+
+import org.eclipse.emf.ecore.xmi.XMLResource;
+import org.eclipse.epsilon.dt.exeed.ExeedEditor;
+import org.mylin.ecore.model.envelope.EnvelopeFactory;
+import org.mylin.ecore.resource.EDIFactResourceFactoryImpl;
+
+public class EDIEditor extends ExeedEditor {
+
+	public EDIEditor() {
+		super();
+		editingDomain.getResourceSet().getResourceFactoryRegistry()
+				.getExtensionToFactoryMap()
+				.put("edi", new EDIFactResourceFactoryImpl(new EclipseEDIRegistry()));
+		editingDomain.getResourceSet().getLoadOptions().put(EDIFactResourceFactoryImpl.FEATURE_IGNORE_NEWLINES, true);
+		editingDomain.getResourceSet().getLoadOptions().put(EDIFactResourceFactoryImpl.FEATURE_VALIDATE, true);
+		editingDomain.getResourceSet().getLoadOptions().put(XMLResource.OPTION_EXTENDED_META_DATA, true);
+		// Initialize Envelope factory
+		EnvelopeFactory.eINSTANCE.getEnvelopePackage();
+	}
+
+}
