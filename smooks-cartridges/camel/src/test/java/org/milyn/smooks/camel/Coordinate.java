642dd6a299d94013e478d54712eb2036e24dae56
danielbevenius <danielbevenius@58448c06-5d2c-0410-b55c-e165fbcd3e60>
2010-10-05 08:18:10 +0000

    Refactored tests.

--- smooks-cartridges/camel/src/test/java/org/milyn/smooks/camel/Coordinate.java
+++ smooks-cartridges/camel/src/test/java/org/milyn/smooks/camel/Coordinate.java
@@ -24,6 +24,14 @@ public class Coordinate {
 	private int x;
 	private int y;
 	
+	public Coordinate()  {
+	}
+	
+	public Coordinate(final int x, final int y)  {
+	    this.x = x;
+	    this.y = y;
+	}
+	
 	public int getX() {
 		return x;
 	}
@@ -38,5 +46,39 @@ public class Coordinate {
 
 	public void setY(int y) {
 		this.y = y;
-	}	
+	}
+
+    @Override
+    public int hashCode()
+    {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + x;
+        result = prime * result + y;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Coordinate other = (Coordinate) obj;
+        if (x != other.x)
+            return false;
+        if (y != other.y)
+            return false;
+        return true;
+    }	
+    
+    public String toString()
+    {
+        return "Coordinate [x=" + x + ", y=" + y + "]";
+    }
+	
+	
 }
